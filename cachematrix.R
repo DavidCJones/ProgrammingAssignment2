b02343f09c6ec6e2461ec7cfecc0b6cb77150836
DavidCJones <dcjoneshome@hotmail.com>
2014-06-14 13:16:24 -0400

    Prog Assign 2 - First R Code Submit to GitHub
    Signed-off-by: DavidCJones <dcjoneshome@hotmail.com>

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cousera R Programming 
+## Second Programing Assignment
+## David Jones (DavidCJones)
+##
+## Example of caching values of time consuming operations in the enclosing environment
+##    Note the assumption is that the matrix can be inversed.
+##
+## Functions 
+##   makeCacheMatrix(<matrix>)  creates a special "matrix" object to cache its inverse in the parrent environment.
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    globInv <- NULL
+    set <- function(y) {
+      x <<- y
+      globInv <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(matInv) globInv <<- matInv
+    getInverse <- function() globInv
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
+  }
+  
 
-}
 
 
-## Write a short comment describing this function
+##   makeCacheSolve(<matrix>)   computes the inverse of the "matrix" returned by `makeCacheMatrix`. If the inverse has
+##                              already been calculated (and the matrix has not changed), then `cacheSolve` should 
+##                              retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+    ## Return a matrix that is the inverse of 'x'
+  
+    globInv <- x$getInverse()
+    if(!is.null(globInv)) {
+      message("getting cached data")
+      return(globInv)
+    }
+    matW <- x$get()
+    globInv <- solve(matW, ...)
+    x$setInverse(globInv)
+    globInv
+  }
+
